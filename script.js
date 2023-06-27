5936104a3f336085a702a4cb53a6c6bac16fc30f
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-26 23:13:07 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const pDescripcion = document.querySelector("#descripcion");
+const aLink = document.querySelector("#link");
+
+function detallesClick() {
+    pDescripcion.innerHTML = "Uso de patito en programacion";
+    aLink.href = "https://es.wikipedia.org/wiki/Método_de_depuración_del_patito_de_goma";
+
+
+}
